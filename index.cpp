f74be47ee2984d72c701807101538ce1c1cd70a7
Joseph-Alexn <167446989+Joseph-Alexn@users.noreply.github.com>
2025-03-09 12:27:09 -0400

    implementacion de lista y optimizacion

--- index.cpp
+++ index.cpp
@@ -147,7 +147,175 @@ public:
     }
 };
 
-Hechizo Entrada(const char *nombreArchivo)
+void magosBajoInvestigacion()
+{
+    ifstream archivo("underInvestigation.in");
+    if (archivo.is_open())
+    {
+        cout << "magos bajo investigacion: " << endl;
+        char nombreMago[100];
+        while (archivo.getline(nombreMago, 100))
+        {
+            cout << nombreMago << endl;
+        }
+        archivo.close();
+    }
+    else
+    {
+        cout << "No se pudo abrir el archivo" << endl;
+    }
+}
+
+bool confluenciaValida(Hechizo &hechizo)
+{
+    int sum = 0;
+    int punto_confluencia = -1;
+    for (int i = 0; i < hechizo.cantidadVertices; i++)
+    {
+        if (hechizo.obtenerVertice(i).runa == 'A')
+        {
+            sum++;
+            punto_confluencia = i;
+        }
+        if (sum > 1)
+        {
+            return false;
+        }
+    }
+    if (punto_confluencia == -1)
+    {
+        return false;
+    }
+    Nodo<Arista> *iterador = hechizo.obtenerVertice(punto_confluencia).aristas.ptr_primero;
+    while (iterador != nullptr)
+    {
+        if (hechizo.obtenerVertice(iterador->valor.vertice_ady).runa != 'B')
+        {
+            return false;
+        }
+        iterador = iterador->ptr_siguiente;
+    }
+    return true;
+}
+
+bool excesoRunasElementales(Hechizo &hechizo)
+{
+    const int max = 3;
+    int sum = 0;
+    for (int i = 0; i < hechizo.cantidadVertices; i++)
+    {
+        if (hechizo.obtenerVertice(i).runa != 'F' && hechizo.obtenerVertice(i).runa != 'A' &&
+            hechizo.obtenerVertice(i).runa != 'B' && hechizo.obtenerVertice(i).runa != 'D')
+        {
+            sum++;
+        }
+        if (sum > max)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+bool runasCataliticasValidas(Hechizo &hechizo)
+{
+    for (int i = 0; i < hechizo.cantidadVertices; i++)
+    {
+        if (hechizo.obtenerVertice(i).runa == 'D')
+        {
+            Nodo<Arista> *iterador = hechizo.obtenerVertice(i).aristas.ptr_primero;
+            while (iterador != nullptr)
+            {
+                if (hechizo.obtenerVertice(iterador->valor.vertice_ady).runa != 'A' &&
+                    hechizo.obtenerVertice(iterador->valor.vertice_ady).runa != 'B' &&
+                    hechizo.obtenerVertice(iterador->valor.vertice_ady).runa != 'F')
+                {
+                    return false;
+                }
+                iterador = iterador->ptr_siguiente;
+            }
+        }
+    }
+    return true;
+}
+bool cicloValido(Vertice &vertice, const int origen)
+{
+    // Funcion auxiliar de DFS_CICLO para chequear si un camino dado es un ciclo valido
+    Nodo<Arista> *iterador = vertice.aristas.ptr_primero;
+    while (iterador != nullptr)
+    {
+        if (iterador->valor.vertice_ady == origen)
+        {
+            return true;
+        }
+        iterador = iterador->ptr_siguiente;
+    }
+    return false;
+}
+
+int DFS_CICLO(Hechizo &hechizo, int vertice, bool visitados[], int longitud, int &longitudMaxima, const int origen)
+{
+    // Implementación de DFS para contar la longitud de un camino y devolver la máxima
+    visitados[vertice] = true;
+    Nodo<Arista> *iterador = hechizo.obtenerVertice(vertice).aristas.ptr_primero;
+    while (iterador != nullptr)
+    {
+        int verticeAdy = iterador->valor.vertice_ady;
+        if (!visitados[verticeAdy])
+        {
+            DFS_CICLO(hechizo, verticeAdy, visitados, longitud + 1, longitudMaxima, origen);
+        }
+        else if (verticeAdy == origen && longitud > 1)
+        {
+            if (cicloValido(hechizo.obtenerVertice(vertice), origen))
+            {
+                if (longitud + 1 > longitudMaxima)
+                {
+                    longitudMaxima = longitud + 1;
+                }
+            }
+        }
+        iterador = iterador->ptr_siguiente;
+    }
+    visitados[vertice] = false;
+    return longitudMaxima;
+}
+
+int encontrarCicloMasLargo(Hechizo &hechizo, int origen)
+{
+    bool *visitados = new bool[hechizo.cantidadVertices];
+    for (int i = 0; i < hechizo.cantidadVertices; i++)
+    {
+        visitados[i] = false;
+    }
+    int longitudMaxima = 0;
+    DFS_CICLO(hechizo, origen, visitados, 0, longitudMaxima, origen);
+    delete[] visitados;
+    return longitudMaxima;
+}
+
+void procesarHechizo(Hechizo &hechizo)
+{
+    cout << "Procesando hechizo de " << hechizo.nombreMago << "..." << endl;
+    if (!confluenciaValida(hechizo))
+    {
+        cout << "Confluencia invalida" << endl;
+    }
+    if (excesoRunasElementales(hechizo))
+    {
+        cout << "Exceso de runas elementales" << endl;
+    }
+    if (!runasCataliticasValidas(hechizo))
+    {
+        cout << "Runas cataliticas invalidas" << endl;
+    }
+
+    int longitudCiclo = encontrarCicloMasLargo(hechizo, 0);
+    cout << "Longitud del ciclo mas largo: " << longitudCiclo << endl;
+
+    cout << "Hechizo procesado" << endl;
+}
+
+void Entrada(const char *nombreArchivo)
 {
     ifstream archivo(nombreArchivo);
     if (!archivo.is_open())
@@ -193,7 +361,7 @@ Hechizo Entrada(const char *nombreArchivo)
             if (archivo.fail())
             {
                 cout << "Error al leer los datos de la arista" << endl;
-                return hechizo;
+                exit(1);
             }
 
             vertice1--;
@@ -209,150 +377,12 @@ Hechizo Entrada(const char *nombreArchivo)
                 cout << "Índice de arista fuera de rango: " << vertice1 + 1 << ", " << vertice2 + 1 << endl;
             }
         }
-
-        cout << "Mago: " << hechizo.nombreMago << endl;
-        cout << "Hechizo: " << hechizo.nombreHechizo << endl;
-        cout << "Cantidad de Vertices: " << hechizo.cantidadVertices << endl;
+        procesarHechizo(hechizo);
     }
 
     archivo.close();
-
-    ifstream archivo2("underInvestigation.in");
-    if (archivo2.is_open())
-    {
-        cout << "magos bajo investigacion: " << endl;
-        char nombreMago[100];
-        while (archivo2.getline(nombreMago, 100))
-        {
-            cout << nombreMago << endl;
-        }
-        archivo2.close();
-    }
-    else
-    {
-        cout << "No se pudo abrir el archivo" << endl;
-    }
-    return Hechizo(0);
+    magosBajoInvestigacion();
 }
-
-bool confluenciaValida(Hechizo hechizo)
-{
-    // Chequear si hay un solo punto de confluencia
-    int sum = 0;
-    int punto_confluencia = 0;
-    for (int i = 0; i < hechizo.cantidadVertices; i++)
-    {
-        if (hechizo.vertices[i].runa == 'A')
-        {
-            sum++;
-            punto_confluencia = i;
-        };
-        if (sum > 1)
-        {
-            return false;
-        }
-    }
-
-    // Chequear si el punto de confluencia tiene solo puntos de soporte energético adyacentes
-    Nodo<Arista> *iterador = hechizo.obtenerVertice(punto_confluencia).aristas.ptr_primero;
-    while (iterador != nullptr)
-    {
-        if (hechizo.obtenerVertice(iterador->valor.vertice_ady).runa != 'B')
-        {
-            return false;
-        }
-        iterador = iterador->ptr_siguiente;
-    }
-    return true;
-}
-
-bool excesoRunasElementales(Hechizo hechizo)
-{
-    // Chequear si hay máximo 3 runas elementales
-    const int max = 3;
-    int sum = 0;
-    for (int i = 0; i < hechizo.cantidadVertices; i++)
-    {
-        // Si la runa no es F, D, A o B, entonces es una runa elemental
-        if (hechizo.obtenerVertice(i).runa != 'F' && hechizo.obtenerVertice(i).runa != 'A' &&
-            hechizo.obtenerVertice(i).runa != 'B' && hechizo.obtenerVertice(i).runa != 'D')
-        {
-            sum++;
-        }
-        if (sum > max)
-        {
-            return true;
-        }
-    }
-    return false;
-}
-
-bool runasCataliticasValidas(Hechizo hechizo)
-{
-    // Chequear si las runas cataliticas no estan conectadas a runas elementales
-    for (int i = 0; i < hechizo.cantidadVertices; i++)
-    {
-        if (hechizo.obtenerVertice(i).runa == 'D')
-        {
-            Nodo<Arista> *iterador = hechizo.obtenerVertice(i).aristas.ptr_primero;
-            while (iterador != nullptr)
-            {
-                if (hechizo.obtenerVertice(iterador->valor.vertice_ady).runa != 'A' &&
-                    hechizo.obtenerVertice(iterador->valor.vertice_ady).runa != 'B' &&
-                    hechizo.obtenerVertice(iterador->valor.vertice_ady).runa != 'F')
-                {
-                    return false;
-                }
-                iterador = iterador->ptr_siguiente;
-            }
-        }
-    }
-    return true;
-}
-
-bool cicloValido(Vertice vertice, const int origen) {
-    // Funcion auxiliar de DFS_CICLO para chequear si un camino dado es un ciclo valido
-    Nodo<Arista> *iterador = vertice.aristas.ptr_primero;
-    while (iterador != nullptr)
-    {
-        if (iterador->valor.vertice_ady == origen)
-        {
-            return true;
-        }
-        iterador = iterador->ptr_siguiente;
-    }
-}
-
-int DFS_CICLO(Hechizo hechizo, int vertice, bool visitados[], int longitud, int& longitudMaxima, const int origen)
-{
-    // Implementación de DFS para contar la longitud de un camino y devolver la máxima
-    visitados[vertice] = true;
-    Nodo<Arista> *iterador = hechizo.obtenerVertice(vertice).aristas.ptr_primero;
-    for (int i = 0; i < hechizo.obtenerVertice(vertice).aristas.longitud; i++)
-    {
-        if (!visitados[iterador->valor.vertice_ady])
-        {
-            DFS_CICLO(hechizo, iterador->valor.vertice_ady, visitados, longitud + 1, longitudMaxima, origen);
-            iterador = iterador->ptr_siguiente;
-        }
-        else
-        {
-            iterador = iterador->ptr_siguiente;
-        }
-    }
-    // Comparar la solucion actual con la longitud maxima encontrada
-    if (cicloValido(hechizo.obtenerVertice(vertice), origen))
-    {
-        if (longitud + 1 > longitudMaxima)
-        {
-            longitudMaxima = longitud + 1;
-        }
-    }
-    // El Back del Tracking
-    visitados[vertice] = false;
-    return longitudMaxima;
-}
-
 int main()
 {
     Entrada("spellList.in");
